c5604b13d26b7d4e7529b9566c2c20fe5bc2a058
sourav-manna <sourav.manna2000@outlook.com>
2022-07-07 18:24:22 +0530

    Sourav Manna | A91005219139 | Amity

--- src/functions-and-arrays.js
+++ src/functions-and-arrays.js
@@ -175,7 +175,7 @@ function maximumProduct(arr){
   for(var i=0;i<arr.length;i++){
     for(var j=0;j<arr.length;j++){
       if (arr[i][j] != 1){
-        break;
+        return 0;
       }
     }
   }return 1;
